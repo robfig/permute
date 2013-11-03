1e0697503124e3b1a3ed88eda1667b703b3f8c77
Rob Figueiredo <robfig@gmail.com>
2013-11-03 17:32:30 -0500

    gc example

---
+++ permute.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void swap_characters(char *first, char *second)
+{
+    char tmp = *first;
+    *first = *second;
+    *second = tmp;
+}
+
+void permute(char array[], int N, int start_index)
+{
+    if (start_index == N - 1)
+    {
+        printf("%s\n", array);
+        return;
+    }
+
+    for (int i = start_index; i < N; i++)
+    {
+        if (start_index != i)
+            swap_characters((array+start_index), (array+i));
+        permute(array, N, start_index+1);
+        swap_characters(array+start_index, array+i);
+    }
+}
+
+/*
+#include <stdlib.h>
+void permute(char array[], int N, int start_index);
+import "C"
+
+func main() {
+	var (
+		input string  = os.Args[1]
+		cstr  *C.char = C.CString(input)
+		N     C.int   = C.int(len(input))
+	)
+	C.permute(cstr, N, C.int(0))
+	C.free(unsafe.Pointer(cstr))
+}
+*/
